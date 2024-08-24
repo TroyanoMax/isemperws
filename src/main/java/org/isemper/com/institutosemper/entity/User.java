cbecd50f46d155f69fb50e1a49bc7048de35f4be
Claudio R. González <claudio.gonzalez@engen.com.mx>
2024-08-24 20:36:06 -0300

    Commit branch

--- src/main/java/org/isemper/com/institutosemper/entity/User.java
+++ src/main/java/org/isemper/com/institutosemper/entity/User.java
@@ -3,14 +3,20 @@ package org.isemper.com.institutosemper.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@EqualsAndHashCode(callSuper = true)
+import java.io.Serial;
+
 @Entity
 @Table(name = "USERS")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class User extends AuditEntity {
+
+    /** * The serial Version UID. */
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
