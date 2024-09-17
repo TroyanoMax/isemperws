690b830cff9211dc04301f5511a2f827e435732d
Claudio R. González <claudio.gonzalez@engen.com.mx>
2024-09-17 18:42:15 -0300

    feature: Alta, Baja y Modificación de Usuarios

--- src/main/java/org/isemper/com/institutosemper/service/UsersServiceImpl.java
+++ src/main/java/org/isemper/com/institutosemper/service/UsersServiceImpl.java
@@ -1,12 +1,17 @@
 package org.isemper.com.institutosemper.service;
 
+import jdk.jshell.spi.ExecutionControl;
 import lombok.extern.log4j.Log4j2;
+import org.isemper.com.institutosemper.exception.GeneralServiceException;
 import org.isemper.com.institutosemper.model.dto.UserResponse;
 import org.isemper.com.institutosemper.model.dto.UserDTO;
+import org.isemper.com.institutosemper.security.model.entity.UserEntity;
 import org.isemper.com.institutosemper.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 
 @Service
@@ -24,8 +29,17 @@ public class UsersServiceImpl implements UsersService {
     public UserResponse userSignup(UserDTO userDTO) {
 
         var users = new UserResponse();
-        users.setUsers(new ArrayList<>());
-        users.getUsers().add(userDTO);
+
+
+        try {
+
+
+
+        } catch (DataAccessException e) {
+            log.error("Error while signing up: {}", e.getMessage());
+            throw new GeneralServiceException("Error while signin up" + e.getMessage());
+        }
+
         return users;
 
     }
