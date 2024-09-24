377f49040b3f5049e52d3deb3ecfa74bccd4e141
Claudio R. González <claudio.gonzalez@engen.com.mx>
2024-09-23 23:52:59 -0300

    feature: Se agregó LoginAuditor

---
+++ src/main/java/org/isemper/com/institutosemper/config/Db1Config.java
@@ -0,0 +1,62 @@
+package org.isemper.com.institutosemper.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Configuration
+@EnableJpaRepositories(
+    basePackages = {
+            "org.isemper.com.institutosemper.security.repository",
+            "org.isemper.com.institutosemper.repository.institutows"
+    },
+    entityManagerFactoryRef = "db1EntityManagerFactory",
+    transactionManagerRef = "db1TransactionManager"
+)
+public class Db1Config {
+
+    @Primary
+    @Bean(name = "db1DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.db1")
+    public DataSource db1DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "db1EntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean db1EntityManagerFactory(
+            @Qualifier("db1DataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan(
+                "org.isemper.com.institutosemper.security.model.entity",
+                "org.isemper.com.institutosemper.model.entity"
+        );
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Primary
+    @Bean(name = "db1TransactionManager")
+    public PlatformTransactionManager db1TransactionManager(
+            @Qualifier("db1EntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        return new JpaTransactionManager(Objects.requireNonNull(entityManagerFactory.getObject()));
+    }
+}
---
+++ src/main/java/org/isemper/com/institutosemper/model/entity/Alumno.java
@@ -0,0 +1,167 @@
+package org.isemper.com.institutosemper.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "alumno", schema = "x031vm05_instituto")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alumno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO", length = 8)
+    private String codigo;
+
+    @Column(name = "N_DOC", length = 8)
+    private String nDoc;
+
+    @Column(name = "T_DOC", length = 2)
+    private String tDoc;
+
+    @Column(name = "APELLIDO", length = 25)
+    private String apellido;
+
+    @Column(name = "NOMBRE", length = 50)
+    private String nombre;
+
+    @Column(name = "DOMICILIO", length = 40)
+    private String domicilio;
+
+    @Column(name = "F_INGRESO")
+    private Date fIngreso;
+
+    @Column(name = "PROVINCIA", length = 30)
+    private String provincia;
+
+    @Column(name = "LOCALIDAD", length = 30)
+    private String localidad;
+
+    @Column(name = "F_NAC")
+    private Date fNac;
+
+    @Column(name = "L_NAC", length = 30)
+    private String lNac;
+
+    @Column(name = "P_NAC", length = 30)
+    private String pNac;
+
+    @Column(name = "N_TEL", length = 40)
+    private String nTel;
+
+    @Column(name = "C_TEL", length = 5)
+    private String cTel;
+
+    @Column(name = "EDAD", length = 11)
+    private String edad;
+
+    @Column(name = "E_CIVIL", length = 2)
+    private String eCivil;
+
+    @Column(name = "NACION", length = 15)
+    private String nacion;
+
+    @Column(name = "SEXO", length = 1)
+    private Character sexo;
+
+    @Column(name = "C_POSTAL", length = 11)
+    private String cPostal;
+
+    @Column(name = "EMAIL", length = 150)
+    private String email;
+
+    @Column(name = "OBSERVA", columnDefinition = "longtext")
+    private String observa;
+
+    @Column(name = "FOTO", columnDefinition = "longtext")
+    private String foto;
+
+    @Column(name = "FOTO_FILE", length = 50)
+    private String fotoFile;
+
+    @Column(name = "FECHA")
+    private Date fecha;
+
+    @Column(name = "SANCION", length = 4)
+    private String sancion;
+
+    @Column(name = "PA_NAC", length = 30)
+    private String paNac;
+
+    @Column(name = "PAIS", length = 30)
+    private String pais;
+
+    @Column(name = "SEC_NOM", length = 30)
+    private String secNom;
+
+    @Column(name = "SEC_LUG", length = 30)
+    private String secLug;
+
+    @Column(name = "SEC_TIT", length = 30)
+    private String secTit;
+
+    @Column(name = "SEC_AÑO", length = 11)
+    private String secAno;
+
+    @Column(name = "UNI_CAR", length = 30)
+    private String uniCar;
+
+    @Column(name = "UNI_NOM", length = 30)
+    private String uniNom;
+
+    @Column(name = "UNI_LUG", length = 30)
+    private String uniLug;
+
+    @Column(name = "UNI_ING", length = 11)
+    private String uniIng;
+
+    @Column(name = "UNI_EST", length = 10)
+    private String uniEst;
+
+    @Column(name = "LAB_HORAS", length = 15)
+    private String labHoras;
+
+    @Column(name = "LAB_EMP", length = 30)
+    private String labEmp;
+
+    @Column(name = "LAB_CAT", length = 30)
+    private String labCat;
+
+    @Column(name = "LAB_REL", length = 7)
+    private String labRel;
+
+    @Column(name = "SOLVENCIA", length = 45)
+    private String solvencia;
+
+    @Column(name = "SOLV_OTRO", length = 30)
+    private String solvOtro;
+
+    @Column(name = "COBERTURA", length = 30)
+    private String cobertura;
+
+    @Column(name = "COB_CAT", length = 12)
+    private String cobCat;
+
+    @Column(name = "ENFERMEDAD", length = 30)
+    private String enfermedad;
+
+    @Column(name = "RELIGION", length = 30)
+    private String religion;
+
+    @Column(name = "ACCION", length = 30)
+    private String accion;
+
+    @Column(name = "COD_BAR", length = 20)
+    private String codBar;
+
+    @Column(name = "COD_BAR1", length = 20)
+    private String codBar1;
+}
